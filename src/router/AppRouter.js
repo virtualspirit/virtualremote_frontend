c6d190176eee5dc42f0d11a6cbedc494b1fb87e6
Santhanu <santhanu.s@shinehub.com.au>
2020-07-08 10:35:29 +0530

    changes done to increase speed

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,53 +1,18 @@
 import React from 'react';
-import { Box, Grommet, ResponsiveContext } from 'grommet';
 import { Route, Switch } from 'react-router';
 import routes from "./routes";
 import PrivateRoute from "./PrivateRoute";
 import '../styles'
 
 const AppRouter = () => {
-    const theme = {
-        global: {
-            focus: {
-                border: {
-                    color: '#00000000'
-                }
-            },
-            colors: {
-                brand: '#574CFA'
-            },
-            // breakpoints: {
-            //     xsmall: {
-            //         value: 375
-            //     },
-            //     small: {
-            //         value: 900,
-            //     },
-            //     large: {
-            //         value: 1024,
-            //     },
-            //     xlarge: {
-            //         value: 1366,
-            //     }
-            // }
-        },
-    };
 
     return (
-        <Grommet plain full theme={theme}>
-            <ResponsiveContext.Consumer>
-                {_ => (
-                    <Box fill>
-                        <Switch>
-                            {routes.map(({ ...object }, k) => (
-                                <PrivateRoute key={k} exact {...object} />
-                            ))}
-                            {/* <Route path="*" component={PageNotFound} /> */}
-                        </Switch>
-                    </Box>
-                )}
-            </ResponsiveContext.Consumer>
-        </Grommet>
+        <Switch>
+            {routes.map(({ ...object }, k) => (
+                <PrivateRoute key={k} exact {...object} />
+            ))}
+            {/* <Route path="*" component={PageNotFound} /> */}
+        </Switch>
     )
 }
 
